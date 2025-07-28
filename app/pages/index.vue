ad6b2367637d116125e527858d11ba42d4e13bc9
Sabrina Ferguson <sf@matterlabs.dev>
2025-07-28 13:29:14 -0400

    configure more nuxt modules

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,12 @@
 <template>
-  <h1>Index page</h1>
+  <div>
+    <h1>{{ $t('welcome') }}</h1>
+    <button v-for="locale in locales" :key="locale" @click="setLocale(locale.code)">
+      {{ locale.name }}
+    </button>
+  </div>
 </template>
+
+<script setup>
+const { locales, setLocale } = useI18n()
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,13 +7,30 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@nuxt/image',
     '@nuxt/test-utils',
-    '@nuxt/ui'
+    '@nuxt/ui',
+    '@nuxtjs/seo',
+    '@nuxtjs/i18n',
+    '@pinia/nuxt'
   ],
+
+  pinia: {
+    storesDirs: ['./stores/**'],
+  },
 
   eslint: {
     config: {
       stylistic: true
     }
   },
+
+  // https://i18n.nuxtjs.org/docs/getting-started/usage
+  // https://nuxtseo.com/docs/site-config/guides/i18n
+  i18n: {
+    baseUrl: 'https://example.com',
+    defaultLocale: 'en',
+    locales: [
+      { code: 'en', name: 'English', file: 'en.json' },
+    ]
+  }
 
 })
